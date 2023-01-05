cb9d6dfaf67d9f897d6b9f46c6f6d75fef112250
Matheus Hora <matheus11hora@gmail.com>
2023-01-05 01:49:10 -0300

    Novos

--- personal_app/lib/pages/home_page.dart
+++ personal_app/lib/pages/home_page.dart
@@ -1,27 +1,129 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:personal_app/models/transaction.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Transaction> transaction = [
+    Transaction(
+      id: '0',
+      title: 'Pagar faculdade',
+      amount: 50,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '1',
+      title: 'Pagar escola',
+      amount: 30,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter app'),
+        backgroundColor: Colors.indigoAccent,
+        title: Text('Minhas despesas'),
+        centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Card(
-            child: Text('data'),
-          ),
-          Card(
-            child: Text('data'),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Card(
+              child: Text('data'),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: const Border(
+                    top: BorderSide(
+                      color: Colors.black12,
+                    ),
+                    left: BorderSide(
+                      color: Colors.black12,
+                    ),
+                    right: BorderSide(
+                      color: Colors.black12,
+                    ),
+                    bottom: BorderSide(
+                      color: Colors.black12,
+                    ),
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ListView.separated(
+                    shrinkWrap: true,
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: transaction.length,
+                    itemBuilder: (_, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Colors.indigoAccent,
+                            child: Text(
+                              '${transaction[index].amount}',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          title: Text(
+                            transaction[index].title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black87,
+                              fontSize: 20,
+                            ),
+                          ),
+                          subtitle: Text(
+                            transaction[index].date.toString(),
+                            style: const TextStyle(
+                              color: Colors.grey,
+                              fontSize: 16,
+                            ),
+                          ),
+                          trailing: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.restore_from_trash_rounded,
+                              color: Colors.blueAccent,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const Divider(
+                        indent: 10,
+                        endIndent: 10,
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
